59a0b64a9e59704a3f5221313d2e3b5a896b3e98
Rezials <suwapat.wow@gmail.com>
2019-11-06 23:41:21 +0700

    Add Movie constructor that take MovieData & implements search logic

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,12 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +24,21 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+		MoviesResponse moviesResponse = movieDataService.fetchAll();
+		List<Movie> movies = new ArrayList<Movie>();
+		for(MovieData movieData: moviesResponse){
+			if(isContain(movieData.getTitle(), queryText)){
+				movies.add(new Movie(movieData));
+			}
+		}
+		return movies;
+	}
+
+	private boolean isContain(String title, String query){
+		if(title.equalsIgnoreCase(query) || query.trim().contains(" ")) return false;
+		String regex = "\\b"+query+"\\b";
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		Matcher m = pattern.matcher(title);
+		return m.find();
 	}
 }
